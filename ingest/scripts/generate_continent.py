0f3ac7f13803833817a71891f802fe102951569c
Cornelius Roemer <cornelius.roemer@gmail.com>
2025-11-07 11:54:09 +0000

    Revert "[pre-commit.ci] auto fixes from pre-commit.com hooks"
    This reverts commit 0be968e2a165f8921c8d5f6b42b92593488cc366.

--- ingest/scripts/generate_continent.py
+++ ingest/scripts/generate_continent.py
@@ -36,7 +36,7 @@ CONTINENT_COUNTRIES = {
         "Saudi Arabia", "Singapore", "South Korea", "Spratly Islands", "Sri Lanka",
         "State of Palestine", "Syria", "Taiwan", "Tajikistan", "Thailand",
         "Timor-Leste", "Turkey", "Turkmenistan", "United Arab Emirates", "Uzbekistan",
-        "Viet Name", "West Bank", "Yemen",
+        "Viet Nam", "West Bank", "Yemen",
     ],
     "Europe": [
         "Albania", "Andorra", "Austria", "Belarus", "Belgium", "Bosnia and Herzegovina",
@@ -179,9 +179,9 @@ def process_records(input_file, output_file):
                             record[key] = ""
                         elif isinstance(value, str) and '\n' in value:
                             record[key] = value.replace('\n', ' ').replace('\r', '')
-
-                    # Replace Viet Name with Vietnam
-                    if record.get('geoLocCountry') == "Viet Name":
+                    
+                    # Replace Viet Nam with Vietnam
+                    if record.get('geoLocCountry') == "Viet Nam":
                         record['geoLocCountry'] = "Vietnam"
 
                     # Write modified record
